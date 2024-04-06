89431f4d3f45d6c4bb0218dad073c623d2769541
Steve Bauman <steven_bauman@outlook.com>
2024-04-06 15:48:07 -0400

    Fix method typehint

--- src/Facades/Bartender.php
+++ src/Facades/Bartender.php
@@ -10,7 +10,7 @@ use DirectoryTree\Bartender\BartenderManager;
  * @method static array handlers()
  * @method static string getUserModel()
  * @method static void setUserModel(string $userModel)
- * @method static void serve(string $driver, string $handler = null)
+ * @method static void serve(string $driver, ?string $handler = null)
  * @method static \Illuminate\Http\RedirectResponse redirect(string $driver)
  * @method static \Illuminate\Http\RedirectResponse callback(string $driver)
  */
